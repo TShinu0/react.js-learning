a99256c75368639df644a5a2e53e23c1c12fcc4f
tshinu0 <tshinu0@gmail.com>
2022-05-25 08:09:48 +0530

    Added Routing Basics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import {Route,Switch} from 'react-router-dom';
 
+// Importing of other pages.
 import AllMeetupPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavouritesPage from './pages/Favourites';
 
+// Function Runs on start because refferd in Index.js
 function App() {
   return (
     <div>
-      <Switch>
-      <Route path={'/'} exact={true}>
+      <Switch>  
+      <Route path={'/'} exact={true}>  
        <AllMeetupPage/> 
         </Route>
         <Route path={'/new-meetup'}>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// imported browser rounting from react for page paths to work.
 import { BrowserRouter } from 'react-router-dom';
 
+// Rendering React - Document-Object-Mode from reactDOM package.
+// using BrowserRouter because it's needed for exact url paths.
+// Using App for the function in the App.js to work.
+// root element is found in the ./public/index.html
 ReactDOM.render(
     <BrowserRouter>
     <App />
