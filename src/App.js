5878375b33008c82a90bf47f8d591b013a4c8f40
tshinu0 <tshinu0@gmail.com>
2022-05-25 08:26:22 +0530

    Added Header Navigation Links to the page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {Route,Switch} from 'react-router-dom';
 import AllMeetupPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavouritesPage from './pages/Favourites';
+import MainNavigation from './components/layout/MainNavigation';
 
 // Function Runs on start because refferd in Index.js
 function App() {
   return (
     <div>
+      <MainNavigation/>
       <Switch>  
       <Route path={'/'} exact={true}>  
        <AllMeetupPage/> 
